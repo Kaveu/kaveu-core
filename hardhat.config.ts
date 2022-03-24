441abe4bc19d173c676b88b829232e6a093eb11b
tetuaoro <tetuaoropro@gmail.com>
2022-03-23 14:29:55 -1000

    remove unnecessary files, configure evm accounts, test task with hardhat, run the tests with the reef local node

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,43 +1,42 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import { task, HardhatUserConfig } from 'hardhat/config'
+import '@reef-defi/hardhat-reef'
+import { ReefNetworkConfig } from '@reef-defi/hardhat-reef/dist/src/types'
 
-dotenv.config();
+dotenv.config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+	const [signer] = await hre.reef.getSigners()
+	await signer.claimDefaultAccount()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+	const bob = await hre.reef.getSignerByName('bob')
+	console.log('bob', await bob.getAddress(), (await bob.getBalance()).toString())
+  await bob.claimDefaultAccount();
+	console.log('bob', await bob.getAddress(), (await bob.getBalance()).toString())
+
+	console.log('signer', await signer.getAddress(), (await signer.getBalance()).toString())
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
+const reef: ReefNetworkConfig = {
+	url: 'ws://localhost:9944',
+	gas: 'auto',
+	gasPrice: 'auto',
+	gasMultiplier: 1,
+	timeout: 10000,
+	httpHeaders: {},
+	accounts: 'remote',
+}
 
-export default config;
+const config: HardhatUserConfig = {
+	solidity: '0.8.4',
+	defaultNetwork: 'reef',
+	networks: { reef },
+}
+
+export default config
