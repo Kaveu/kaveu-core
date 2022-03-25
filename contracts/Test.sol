50c8dcf13aaf57af900a20c8683fa715ab0ef855
tetuaoro <tetuaoropro@gmail.com>
2022-03-25 09:43:31 -1000

    torumoniERC721 to ERC1155

--- contracts/Test.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-import "../sqwid-core/contracts/SqwidERC1155.sol";
-
-contract TestERC1155 is SqwidERC1155 {}
--- contracts/Torumoni.sol
+++ contracts/Torumoni.sol
@@ -2,47 +2,57 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TorumoniERC721 is ERC721, Ownable {
-    uint256 public constant maxSupply = 7;
+/// Crowdfunding
+contract TorumoniERC1155 is ERC1155, Ownable {
+    using Strings for uint256;
+
+    uint256 public constant MAX_SUPPLY = 17;
     uint256 public costClaw;
     mapping(uint256 => uint256) private _claws;
+    string private _baseUri;
 
-    constructor() ERC721("Torumoni", "MONI") {
+    constructor(string memory uri_) ERC1155(uri_) {
         costClaw = 1 ether;
-        for (uint256 tokenId = 1; tokenId <= maxSupply; tokenId++) {
-            _mint(msg.sender, tokenId);
-            if (tokenId == 1) {
-                _claws[tokenId] = 101;
-            } else {
-                _claws[tokenId] = 2;
-            }
+        _baseUri = uri_;
+        uint256[] memory ids;
+        uint256[] memory amounts;
+        for (uint256 i = 0; i < MAX_SUPPLY; i++) {
+            ids[i] = i + 1;
+            amounts[i] = 1;
         }
+        _mintBatch(msg.sender, ids, amounts, "MONI");
     }
 
-    function clawsOf(uint256 tokenId) public pure returns (uint256) {
-        require(_exists(tokenId), "Torumoni: TOKEN NOT EXISTS");
+    function name() public pure returns (string memory) {
+        string memory collection = "Torumoni";
+        return collection;
+    }
 
+    function uri(uint256 _tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(_baseUri, Strings.toString(_tokenId), ".json"));
+    }
+
+    function setBaseUri(string memory baseUri_) public onlyOwner {
+        _baseUri = baseUri_;
+    }
+
+    function clawsOf(uint256 tokenId) public view returns (uint256) {
+        // require(_exists(tokenId), "Torumoni: TOKEN NOT EXISTS");
         return _claws[tokenId];
-    }
-
-    /**
-        @dev See {ERC721-_baseURI}.
-     */
-    function _baseURI() internal view virtual override returns (string memory) {
-        return "";
     }
 
     /**
         @dev Set +3 `_claws` of owners except the creator.
      */
     function airdrop() public onlyOwner {
-        for (uint256 tokenId = 2; tokenId <= maxSupply; tokenId++) {
-            if (ownerOf(tokenId) != owner()) {
+        for (uint256 tokenId = 2; tokenId <= MAX_SUPPLY; tokenId++) {
+            // if (ownerOf(tokenId) != owner()) {
                 _claws[tokenId] += 3;
-            }
+            // }
         }
     }
 
@@ -57,12 +67,7 @@ contract TorumoniERC721 is ERC721, Ownable {
         @dev See {@openzeppelin-utils/Address.sol}.
      */
     function withdraw() public payable onlyOwner {
-        (bool success, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(
-            success,
-            "Address: unable to send value, sender may have reverted"
-        );
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Torumoni : WITHDRAW FAILED");
     }
 }
