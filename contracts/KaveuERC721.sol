ef27ddc717784e21ea8e65f6866ca61946a8ae7f
tetuaoro <tetuaoropro@gmail.com>
2022-04-03 15:47:00 -1000

    Update : add functions to assign borrower

--- contracts/KaveuERC721.sol
+++ contracts/KaveuERC721.sol
@@ -122,51 +122,156 @@ contract KaveuERC721 is ERC721, ERC721Holder, Ownable {
     /////////////////////////// LOAN ////////////////////////////////////
     /////////////////////////////////////////////////////////////////////
 
-    // allow `maxClaw` to loan for `pricePerDay`
-    struct LoanData {
+    struct ClawLoan {
         uint256 pricePerDay;
-        uint256 maxClaw;
-        uint256 totalLoaners;
+        uint256 maxLoan;
+        uint256 totalBorrow;
     }
 
-    struct LoanDate {
-        uint64 startedAt;
-        uint64 _nbDays;
+    enum AssignState {
+        DEFAULT,
+        BY_OWNER,
+        BY_BORROWER
     }
 
-    mapping(uint256 => LoanData) private _loan;
-    mapping(uint256 => mapping(address => LoanDate)) private _loaners;
+    struct ClawBorrow {
+        uint256 startedAt;
+        uint256 endAt;
+        AssignState assigned;
+    }
 
-    function createLoan(
-        uint256 _tokenId,
-        uint256 _maxClaw,
-        uint256 _pricePerDay
-    ) external {
-        LoanData storage ld = _loan[_tokenId];
+    event ClawBorrowed(uint256 indexed tokenId, address indexed borrower);
 
-        if (_pricePerDay > 0) {
-            ld.pricePerDay = _pricePerDay;
+    uint256 constant DAY_IN_SECONDS = 86400;
+    mapping(uint256 => ClawLoan) private _clawLoans;
+    mapping(uint256 => mapping(address => ClawBorrow)) private _borrowers;
+    address[] private claw_borrow_result;
+
+    modifier onlyOwnerOf(uint256 tokenId) {
+        require(ownerOf(tokenId) == msg.sender, "Kaveu721: you are not the owner");
+        _;
+    }
+
+    /**
+     * @dev Returns the `ClawLoan` for a `_tokenId`.
+     */
+    function loanOf(uint256 _tokenId) external view returns (ClawLoan memory) {
+        ClawLoan memory cl = _clawLoans[_tokenId];
+        return cl;
+    }
+
+    /**
+     * @dev Returns the `ClawBorrow` for a `_tokenId`. The bot uses this function to check if the `_borrower` is allowed to use the bot.
+     * More info on website.
+     */
+    function borrowOf(uint256 _tokenId) external view returns (ClawBorrow[] memory) {
+        ClawBorrow[] memory cbs;
+        uint256 cbsIndex = 0;
+        for (uint256 i = 0; i < claw_borrow_result.length; i++) {
+            if (_borrowers[_tokenId][claw_borrow_result[i]].assigned != AssignState.DEFAULT) {
+                cbs[cbsIndex] = _borrowers[_tokenId][claw_borrow_result[i]];
+                cbsIndex++;
+            }
         }
-        ld.maxClaw = _maxClaw;
-
-        require(ld.totalLoaners <= ld.maxClaw && ld.maxClaw <= _claws[_tokenId] && ownerOf(_tokenId) == msg.sender, "Kaveu721: cannot create loan");
+        return cbs;
     }
 
-    function fundLoan(
+    /**
+     * @dev Manually assigns a `_borrower` to the `_tokenId` without paying the loan fee for 365 days.
+     * More info on website.
+     */
+    function assign(uint256 _tokenId, address _borrower) external onlyOwnerOf(_tokenId) {
+        ClawLoan storage cl = _clawLoans[_tokenId];
+        ClawBorrow storage cb = _borrowers[_tokenId][_borrower];
+        cl.totalBorrow += 1;
+
+        require(cl.totalBorrow <= _claws[_tokenId] && cb.assigned == AssignState.DEFAULT, "Kaveu721: cannot assign the borrower");
+
+        cb.startedAt = block.timestamp;
+        cb.endAt = block.timestamp + (365 * DAY_IN_SECONDS);
+        cb.assigned = AssignState.BY_OWNER;
+        claw_borrow_result.push(_borrower);
+
+        emit ClawBorrowed(_tokenId, _borrower);
+    }
+
+    /**
+     * @dev Deassigns a `_borrower` who has already been manually assigned from the `_tokenId`.
+     */
+    function deassign(uint256 _tokenId, address _borrower) external onlyOwnerOf(_tokenId) {
+        ClawLoan storage cl = _clawLoans[_tokenId];
+        cl.totalBorrow -= 1;
+
+        require(_borrowers[_tokenId][_borrower].assigned == AssignState.BY_OWNER, "Kaveu721: cannot deassign the borrower");
+        uint256[] memory array;
+        uint256 arrayIndex = 0;
+        uint256 i;
+        for (i = 0; i < claw_borrow_result.length; i++) {
+            if (claw_borrow_result[i] == _borrower) {
+                array[arrayIndex] = i;
+                arrayIndex++;
+                delete _borrowers[_tokenId][_borrower];
+                break;
+            }
+        }
+        for (i = 0; i < array.length; i++) {
+            delete claw_borrow_result[array[i]];
+        }
+    }
+
+    function loan(
         uint256 _tokenId,
-        uint256 _nbClaw,
-        uint256 _nbDays
+        uint256 _maxLoan,
+        uint256 _pricePerDay
+    ) external onlyOwnerOf(_tokenId) {
+        ClawLoan storage cl = _clawLoans[_tokenId];
+        cl.pricePerDay = _pricePerDay;
+        cl.maxLoan = _maxLoan;
+        require(cl.totalBorrow <= cl.maxLoan && cl.maxLoan <= _claws[_tokenId], "Kaveu721: cannot create loan");
+    }
+
+    function cleanUp(uint256 _tokenId) external onlyOwnerOf(_tokenId) {
+        uint256[] memory array;
+        uint256 arrayIndex = 0;
+        uint256 i;
+        for (i = 0; i < claw_borrow_result.length; i++) {
+            if (_borrowers[_tokenId][claw_borrow_result[i]].endAt < block.timestamp) {
+                array[arrayIndex] = i;
+                arrayIndex++;
+                delete _borrowers[_tokenId][claw_borrow_result[i]];
+            }
+        }
+        for (i = 0; i < array.length; i++) {
+            delete claw_borrow_result[array[i]];
+        }
+    }
+
+    function borrow(
+        uint256 _tokenId,
+        uint256 _forClaw,
+        uint256 _forDays,
+        address _borrower
     ) external payable {
-        LoanData storage ld = _loan[_tokenId];
+        /* ClawLoan storage cl = _clawLoans[_tokenId];
+        cl.totalBorrow += _forClaw;
 
-        ld.totalLoaners += _nbClaw;
-        LoanDate memory ldate;
-        ldate.startedAt = now;
-        ldate.nbDays = _nbDays;
-        _loaners[_tokenId][msg.sender] = ldate;
+        ClawBorrow storage cb = _borrowers[_tokenId][_borrower];
 
-        require(msg.value >= _nbClaw * _nbDays * ld.pricePerDay, "Kaveu721: not enought token");
+        if (cb.assigned == AssignState.DEFAULT) {
+            cb.startedAt = block.timestamp;
+            cb.endAt = block.timestamp + (_forDays * DAY_IN_SECONDS);
+        }
+
+        require(cl.totalBorrow <= cl.maxLoan && cl.maxLoan <= _claws[_tokenId], "Kaveu721: cannot create loan");
+
+        // pays loan fee
+        require(msg.value >= _forClaw * _forDays * cl.pricePerDay, "Kaveu721: not enought token");
         (bool success, ) = payable(ownerOf(_tokenId)).call{value: msg.value}("");
         require(success, "Address: unable to send value");
+
+        cb.assigned = AssignState.BY_BORROWER;
+        claw_borrow_result.push(_borrower);
+
+        emit ClawBorrowed(_tokenId, _borrower); */
     }
 }
